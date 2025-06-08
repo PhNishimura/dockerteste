88906afc633728742550b7e38085b68a3f62a118
Pedro Nishimura <129108608+PhNishimura@users.noreply.github.com>
2025-06-08 19:08:56 -0300

    super teste

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,14 @@
+```vue
+<template>
+  <main>
+    <section class="banner">
+      <h1 class="banner__title">All Office Products</h1>
+      <h2 class="banner__subtitle">Under One Roof</h2>
+      <p class="banner__text">Explore a variedade de materiais de escritório.</p>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+</script>
+```
